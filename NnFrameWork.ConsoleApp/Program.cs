cc8aad8f28070372b0af1c2c2fd5cb6daf560f58
liuxiang <806685981@qq.com>
2017-11-11 21:09:44 +0800

    init

---
+++ NnFrameWork.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NnFrameWork.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
